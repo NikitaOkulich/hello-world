4699e5c35b6415d2e5b9a0b5b412518ac100211f
NikitaOkulich <54985365+NikitaOkulich@users.noreply.github.com>
2019-09-11 15:42:59 +0300

    Add files via upload

---
+++ Home_Work2.java
@@ -0,0 +1,34 @@
+package Home_Work2;
+
+public class Home_Work2 {
+
+public static void main(String[] args) {
+    char myChar = 33;
+    System.out.println(myChar);
+short myShort = 123;
+short myShort1 = 1000;
+System.out.println(myChar+myShort-myShort1);
+boolean myBoolean = false;
+System.out.println(myBoolean);
+int myInt = 23;
+byte myByte = (byte) myInt;
+System.out.println(myByte);
+int a =4;
+double b = (double) (a);
+System.out.println(b);
+double c = 3.5;
+int w = (int) (c);
+System.out.println(w);
+int t = 24;
+String k = Integer.toString(t);
+System.out.println(k);
+int g = 3;
+int e = 2;
+    System.out.println(g / e);
+double z = 6.4;
+double r = 4.3;
+    System.out.println(z / r );
+System.out.println("4 << 2" + (4<<2));
+    System.out.println("4 >>2" + (4>>2));
+}
+}
